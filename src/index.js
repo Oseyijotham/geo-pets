bee81721fa0e691ebec47075c7376324071dfc15
Oseyijotham <jothamoboh16@gmail.com>
2025-09-16 20:20:45 +0100

    Update App

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="Airboxify">
+      <BrowserRouter basename="geo-pets">
         <App />
       </BrowserRouter>
     </Provider>
