55cb7fdd26092328756475a2a2c7512ba68974a8
Oseyijotham <jothamoboh16@gmail.com>
2025-10-08 09:13:05 +0100

    Update App

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -5,6 +5,8 @@ import {
   selectContactsFilter,
   selectFilterUp,
   selectFilterDown,
+  selectIsLoading,
+  selectIsUpdateLoading,
 } from '../../redux/AppRedux/selectors';
 import { useDispatch } from 'react-redux';
 import { nanoid } from 'nanoid';
@@ -17,8 +19,12 @@ import {
   updateStatus,
 } from '../../redux/AppRedux/operations';
 import icons from './icons.svg';
+import { ThreeCircles } from 'react-loader-spinner';
 
 export const Filter = () => {
+  const isLoading = useSelector(selectIsLoading);
+  const isUpdateLoading = useSelector(selectIsUpdateLoading);
+
   const [isTrue, setIfTrue] = useState(true);
   const searchTermId = nanoid();
   const places = useSelector(selectPlaces);
@@ -73,53 +79,91 @@ export const Filter = () => {
         />
       </label>
 
-      {filterValue !== '' && bestMatches.length !== 0 && (
-        <ul className={css.contactsList} style={{ height: '315px' }}>
-          {bestMatches.map(place => {
-            const myindex = bestMatches.indexOf(place);
-            if (myindex >= filterLower && myindex < filterUpper) {
-              return (
-                <li
-                  key={place.id}
-                  data-id={place.id}
-                  className={css.contactsItem}
-                  onClick={handleModalOpen}
-                >
-                  <span className={css.contactsData} data-id={place.id}>
-                    <input
-                      type="checkbox"
-                      className={css.checkbox}
-                      onChange={evt => handleChange(place, evt)}
-                      name={place.id}
-                      checked={place.status}
-                      value={place.properties.addresses[0]}
-                    />
-                    :{' '}
-                    <span className={css.contactsPhone} data-id={place.id}>
-                      {place.properties.names.primary}
-                    </span>
-                  </span>
-                  {place.properties.socials.length !== 0 && (
-                    <span className={css.contactsButtonArea}>
-                      <a
-                        className={css.contactsButton}
+      <div style={{ position: 'relative' }}>
+        {(isLoading || isUpdateLoading) && filterValue !== '' && (
+          <div className={css.backDrop}>
+            <div className={css.centerStyle}>
+              <ThreeCircles
+                visible={true}
+                height="60"
+                width="60"
+                color="#9225ff"
+                radius="9"
+                ariaLabel="three-dots-loading"
+                wrapperStyle={{}}
+                wrapperClass={css.loader}
+              />
+              {isLoading && (
+                <p className={css.centerLabel}>
+                  Please be patient, fetching places can take up to 60 seconds
+                </p>
+              )}
+              {isUpdateLoading && isTrue === true && (
+                <p className={css.centerLabel}>
+                  Saving place to your API Database
+                </p>
+              )}
+              {isUpdateLoading && isTrue === false && (
+                <p className={css.centerLabel}>
+                  Removing place from your API Database
+                </p>
+              )}
+            </div>
+          </div>
+        )}
+
+        {filterValue !== '' && bestMatches.length !== 0 && (
+          <ul className={css.contactsList} style={{ height: '315px' }}>
+            {bestMatches.map(place => {
+              const myindex = bestMatches.indexOf(place);
+              if (myindex >= filterLower && myindex < filterUpper) {
+                return (
+                  <li
+                    key={place.id}
+                    data-id={place.id}
+                    className={css.contactsItem}
+                    onClick={handleModalOpen}
+                  >
+                    <span className={css.contactsData} data-id={place.id}>
+                      <input
+                        type="checkbox"
+                        className={css.checkbox}
+                        onChange={evt => handleChange(place, evt)}
                         name={place.id}
-                        href={place.properties.socials[0]}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        <svg width="20" height="20" className={css.socialsIcon}>
-                          <use href={`${icons}#icon-facebook`} />
-                        </svg>
-                      </a>
+                        checked={place.status}
+                        value={place.properties.addresses[0]}
+                      />
+                      :{' '}
+                      <span className={css.contactsPhone} data-id={place.id}>
+                        {place.properties.names.primary}
+                      </span>
                     </span>
-                  )}
-                </li>
-              );
-            }
-          })}
-        </ul>
-      )}
+                    {place.properties.socials.length !== 0 && (
+                      <span className={css.contactsButtonArea}>
+                        <a
+                          className={css.contactsButton}
+                          name={place.id}
+                          href={place.properties.socials[0]}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          <svg
+                            width="20"
+                            height="20"
+                            className={css.socialsIcon}
+                          >
+                            <use href={`${icons}#icon-facebook`} />
+                          </svg>
+                        </a>
+                      </span>
+                    )}
+                  </li>
+                );
+              }
+            })}
+          </ul>
+        )}
+      </div>
     </div>
   );
 };
